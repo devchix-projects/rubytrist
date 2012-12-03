8b082a285f06c6622be66b54f649c80e63c14477
rubygeek <nola@rubygeek.com>
2012-12-02 21:10:13 -0600

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubytrist::Application.config.secret_token = 'c3b8528d0103f1fb62bdb3d0aef0d86e3db77a83c6f7531dab441a0e49e89c1c841edd035e6f227921366da5c81c564c13e4d06a923e6cf4f7f4f322f099df72'
