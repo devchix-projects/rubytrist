5180d998ab9e056bf05144fbdac13b1cadfb03ff
rubygeek <nola@rubygeek.com>
2012-12-16 21:26:33 -0600

    progress on nopassword for historical purposes if anyone wants to see it

--- Gemfile
+++ Gemfile
@@ -1,10 +1,11 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
-
 gem 'nopassword', :git => 'https://github.com/alsmola/nopassword.git'
-
-
+# to work on a gem you have checked out locally
+#gem 'nopassword', :path => '../nopassword'
+gem 'sqlite3'
+gem 'jquery-rails'
 gem 'therubyracer'
 gem 'libv8', '~> 3.11.8'
  
@@ -12,10 +13,6 @@ group :assets do
   gem 'twitter-bootstrap-rails'
   gem "less-rails"
 end
-
-gem 'sqlite3'
-
-gem 'jquery-rails'
 
 group :test, :development do 
 	gem 'rspec-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rubytrist::Application.routes.draw do
-  get "home/index"
+  root :to => 'home#index'
 
+  get "home/index"
+  mount Nopassword::Engine, :at => "/nopassword"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +53,6 @@ Rubytrist::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
